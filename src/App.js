a3a218f64e104fe2173323bce30ae3cd916fc7e3
Micke Berg <micke.berg.dev@gmail.com>
2020-12-13 22:17:00 +0100

    change to scss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from "axios";
-import './App.css';
+import './App.scss';
 import List from './Components/List/List';
 import Spinner from './Components/Spinner/Spinner';
 import Pagination from './Components/Pagination/Pagination';
